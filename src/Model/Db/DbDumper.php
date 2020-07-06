52cc3c16116ee557d3336d401924b3ade05ebe5e
Diego Cabrejas <diego@wearejh.com>
2020-07-06 19:42:03 +0100

    Change cron to use the new facade. Refactor to remove dependency on mysqldump.

--- src/Model/Db/DbDumper.php
+++ src/Model/Db/DbDumper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Jh\StrippedDbProvider\Model\Db;
 
+use Ifsnop\Mysqldump\Mysqldump;
 use Magento\Framework\Shell;
 use Magento\Framework\Config\ConfigOptionsListConstants;
 use Jh\StrippedDbProvider\Model\Config;
@@ -11,8 +12,6 @@ use Jh\StrippedDbProvider\Model\ProjectMeta;
 
 class DbDumper
 {
-    private $rmDefinerCommand = "LANG=C LC_CTYPE=C LC_ALL=C sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/'";
-    private $rmPasswordWarning = "grep -v 'Warning: Using a password'";
     /**
      * @var Shell
      */
@@ -44,66 +43,27 @@ class DbDumper
      */
     public function dumpDb(ProjectMeta $projectMeta): void
     {
-        $this->shell->execute($this->buildStructureDumpForStrippedTablesCmd($projectMeta));
-        $this->shell->execute($this->buildDataDumpExcludingStrippedTablesCmd($projectMeta));
-    }
-
-    /**
-     * @return string
-     */
-    private function buildDataDumpExcludingStrippedTablesCmd(ProjectMeta $projectMeta): string
-    {
-        $dumpCmd = "mysqldump --single-transaction --quick -h%s -u%s --password=%s %s %s | %s | %s >> %s";
-        return sprintf(
-            $dumpCmd,
-            $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_HOST),
+        $hostName = $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_HOST);
+        $dbName   = $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_NAME);
+        $dumper = new Mysqldump(
+            "mysql:host={$hostName};dbname={$dbName}",
             $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_USER),
             $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_PASSWORD),
-            $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_NAME),
-            $this->buildListOfStructureOnlyTables(true),
-            $this->rmPasswordWarning,
-            $this->rmDefinerCommand,
-            $projectMeta->getLocalAbsoluteFileDumpPath()
+            ['skip-definer' => true]
         );
+
+        $dumper->setTableLimits($this->getTableLimits());
+        $dumper->start($projectMeta->getLocalAbsoluteFileDumpPath());
     }
 
-    /**
-     * @return string
-     */
-    private function buildStructureDumpForStrippedTablesCmd(ProjectMeta $projectMeta): string
-    {
-        $dumpCmd = "mysqldump --single-transaction --quick --no-data -h%s -u%s --password=%s %s %s | %s | %s > %s";
-        return sprintf(
-            $dumpCmd,
-            $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_HOST),
-            $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_USER),
-            $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_PASSWORD),
-            $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_NAME),
-            $this->buildListOfStructureOnlyTables(false),
-            $this->rmPasswordWarning,
-            $this->rmDefinerCommand,
-            $projectMeta->getLocalAbsoluteFileDumpPath()
-        );
-    }
-
-    /**
-     * @param bool $ignoreTableFlag
-     * @return string
-     */
-    private function buildListOfStructureOnlyTables(bool $ignoreTableFlag = false): string
+    private function getTableLimits(): array
     {
         $tableNameList = $this->dbTables->getStructureOnlyTables();
-        if ($ignoreTableFlag) {
-            $dbName = $this->config->getLocalDbConfigData(ConfigOptionsListConstants::KEY_NAME);
-            $tableNameList = array_map(function ($tableName) use ($dbName) {
-                return sprintf(
-                    "--ignore-table=%s.%s",
-                    $dbName,
-                    $tableName
-                );
-            }, $tableNameList);
+        $tableLimits = [];
+        foreach ($tableNameList as $tableName) {
+            $tableLimits[$tableName] = 0;
         }
 
-        return implode(" ", $tableNameList);
+        return $tableLimits;
     }
 }
